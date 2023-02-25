f26f602b6a44f08a6e12a19743476c43c470f467
Napiorski <michaeldnapiorski@gmail.com>
2023-02-24 23:33:22 -0800

    finished the tutorial, lots of bugs

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
 };
 
-module.exports = nextConfig;
+(module.exports = nextConfig),
+  (module.exports = {
+    serverRuntimeConfig: {
+      secret:
+        "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING",
+    },
+    publicRuntimeConfig: {
+      apiUrl:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000/api" // development api
+          : "http://localhost:3000/api", // production api
+    },
+  });

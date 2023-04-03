00eaef744fba3d43063b2e759debad957d931cb9
Napiorski <michaeldnapiorski@gmail.com>
2023-04-03 08:56:39 -0700

    updated bullet points and additional info

--- components/additional-info/index.tsx
+++ components/additional-info/index.tsx
@@ -1,4 +1,5 @@
-import { GridItem, Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, useListStyles } from "@chakra-ui/react";
+import { GiCheckedShield } from "react-icons/gi";
 import * as React from "react";
 
 type AdditionalInfoProps = {
@@ -8,18 +9,108 @@ type AdditionalInfoProps = {
 function AdditionalInfo({ data }: AdditionalInfoProps) {
   return (
     <GridItem colSpan={8}>
-      <div>HP: {data.hit_points}</div>
-      <div>Difficulty: {data.challenge_rating}</div>
-      <div>XP: {data.xp}</div>
-      <div>Walk Speed: {data.speed.walk}</div>
-      <div>Swim Speed: {data.speed.swim}</div>
-      <div>Fly Speed: {data.speed.fly}</div>
-      <div>STR: {data.strength}</div>
-      <div>DEX: {data.dexterity}</div>
-      <div>CON: {data.constitution}</div>
-      <div>INT: {data.intelligence}</div>
-      <div>WIS: {data.wisdom}</div>
-      <div>CHA: {data.charisma}</div>
+      <Grid templateColumns="repeat(8, 1fr)" gap={4}>
+        <GridItem colSpan={2}>
+          <Heading as="h6" size="md">
+            Name:
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.name}</span>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Heading as="h6" size="md">
+            Type:
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.type}</span>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Heading as="h6" size="md">
+            Armor Class:
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={6}>
+          <ul style={{ listStyleType: "none" }}>
+            {data.armor_class.map(
+              (ac: { type: number; value: string }, i: number) => {
+                return (
+                  <li key={`armor-class-item-${i}`}>
+                    <GiCheckedShield style={{ display: "inline-block" }} />
+                    <span>{" " + ac.type + " " + ac.value}</span>
+                  </li>
+                );
+              }
+            )}
+          </ul>
+        </GridItem>
+        <GridItem colSpan={2}>Size:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.size}</span>
+        </GridItem>
+        <GridItem colSpan={2}>HP:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.hit_points}</span>
+        </GridItem>
+        <GridItem colSpan={2}>Difficulty:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.challenge_rating}</span>
+        </GridItem>
+        <GridItem colSpan={2}>XP:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.xp}</span>
+        </GridItem>
+        {data.speed.walk && (
+          <>
+            <GridItem colSpan={2}>Walk Speed:</GridItem>
+
+            <GridItem colSpan={6}>
+              <span>{data.speed.walk}</span>
+            </GridItem>
+          </>
+        )}
+        {data.speed.swim && (
+          <>
+            <GridItem colSpan={2}>Swim Speed:</GridItem>
+            <GridItem colSpan={6}>
+              <span>{data.speed.swim}</span>
+            </GridItem>
+          </>
+        )}
+        {data.speed.fly && (
+          <>
+            <GridItem colSpan={2}>Fly Speed:</GridItem>
+            <GridItem colSpan={6}>
+              <span>{data.speed.fly}</span>
+            </GridItem>
+          </>
+        )}
+        <GridItem colSpan={2}>STR:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.strength}</span>
+        </GridItem>
+        <GridItem colSpan={2}>DEX:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.dexterity}</span>
+        </GridItem>
+        <GridItem colSpan={2}>CON:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.constitution}</span>
+        </GridItem>
+        <GridItem colSpan={2}>INT:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.intelligence}</span>
+        </GridItem>
+        <GridItem colSpan={2}>WIS:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.wisdom}</span>
+        </GridItem>
+        <GridItem colSpan={2}>CHA:</GridItem>
+        <GridItem colSpan={6}>
+          <span>{data.charisma}</span>
+        </GridItem>
+      </Grid>
       <div>
         <br />
         <hr />

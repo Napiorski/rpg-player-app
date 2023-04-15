a45646365da79ba81b8ebb1ecf9fa8d1e141c3ed
jasonwr <jsnrice@gmail.com>
2023-04-15 17:42:14 -0600

    Protected routes with local storage access token setup code.

--- context/providers/app-provider.tsx
+++ context/providers/app-provider.tsx
@@ -1,22 +1,18 @@
 import * as React from "react";
 
 export const AppContext = React.createContext({
-  user: "",
-  login: (userInfo: string) => {},
   logout: () => {},
+  username: "",
+  setUsername: (username: string) => {},
 });
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState<string>("");
-
-  function login(userInfo: string) {
-    setUser(userInfo);
-  }
+  const [username, setUsername] = React.useState<string>("");
 
   function logout() {
-    setUser("");
+    setUsername("");
   }
 
-  const value = React.useMemo(() => ({ user, login, logout }), [user]);
+  const value = React.useMemo(() => ({ logout, username, setUsername }), [username]);
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 }
